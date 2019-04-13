97536628db72f9ec18279c42e8bbe7b2a299fd41
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-13 20:38:42 -0300

    Indent code

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
-    <div class="title my-4"
-      @click="onCategoriesTitleClick">
-      <h1>{{ title }}</h1>
-      <i
-        class="ml-3 fas"
-        :class="{ 'fa-plus': !showCategories, 'fa-minus': showCategories }"
-      >
-      </i>
+    <div class="title my-4" @click="onCategoriesTitleClick">
+      <h2>{{ title }}</h2>
+      <i class="ml-3 fas" :class="{ 'fa-plus': !showCategories, 'fa-minus': showCategories }"></i>
     </div>
     <div v-if="showCategories">
       <ul class="list-group" v-if="categories.length > 0">
-        <gws-category
-          v-for="(category, index) in categories"
-          :key="index"
-          :category="category"
-          >
-        </gws-category>
+        <gws-category v-for="(category, index) in categories" :key="index" :category="category" customClasses="list-group-item"></gws-category>
       </ul>
       <h4 class="text-center text-muted" v-else>No categories found =/</h4>
     </div>
@@ -24,7 +14,7 @@
 </template>
 
 <script>
-import Category from '@/components/Category.vue'
+import Category from '@/components/Category.vue';
 
 export default {
   props: {
@@ -37,20 +27,20 @@ export default {
       required: true
     }
   },
-  data () {
+  data() {
     return {
       showCategories: true
-    }
+    };
   },
   components: {
     gwsCategory: Category
   },
   methods: {
-    onCategoriesTitleClick () {
-      this.showCategories = !this.showCategories
+    onCategoriesTitleClick() {
+      this.showCategories = !this.showCategories;
     }
   }
-}
+};
 </script>
 
 <style scoped>
