9fe66e419cea3aaa2dd71f2d817523e5dbf8be54
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-13 18:41:30 -0300

    Add sample social media links

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="social-media text-muted">
-    <i class="fab fa-facebook-f"></i>
-    <i class="fab fa-instagram"></i>
-    <i class="fab fa-twitter"></i>
+    <a href="https://www.facebook.com" title="Facebook" target="_blank" rel="noreferrer noopener"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://www.instagram.com" title="Instagram" target="_blank" rel="noreferrer noopener"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.twitter.com" title="Twitter" target="_blank" rel="noreferrer noopener"><i class="fab fa-twitter"></i></a>
   </div>
 </template>
 
@@ -11,14 +11,17 @@
   display: flex;
   align-content: center;
   align-items: center;
+  justify-content: center;
 }
 
-.social-media i {
+.social-media a {
+  text-decoration: none;
   margin: 0 0.5rem;
+  color: #6c757d;
 }
 
-.social-media i:hover,
-.social-media i:active {
+.social-media a:hover,
+.social-media a:active {
   color: #fff;
 }
 </style>
