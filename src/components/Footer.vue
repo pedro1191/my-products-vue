683f1a00364324f8ac527b13bb9642b5ca931dcd
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-13 18:30:41 -0300

    Update app name, logo and theme color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,8 @@
     <div class="container">
       <div class="row">
         <div class="col d-flex">
-          <p class="m-0 text-muted">Copyright &copy; My Products {{ thisYear }}</p>
-          <gws-social-media class="ml-auto"/>
+          <p class="m-0 text-muted">Copyright &copy; My Food {{ thisYear }}</p>
+          <gws-social-media class="ml-auto" />
         </div>
       </div>
     </div>
@@ -12,16 +12,16 @@
 </template>
 
 <script>
-import SocialMedia from '@/components/SocialMedia.vue'
+import SocialMedia from './SocialMedia.vue';
 
 export default {
   computed: {
-    thisYear () {
-      return new Date().getFullYear()
+    thisYear() {
+      return new Date().getFullYear();
     }
   },
   components: {
     gwsSocialMedia: SocialMedia
   }
-}
+};
 </script>
