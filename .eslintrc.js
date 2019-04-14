c18e48ef846819a1f8804fb4a726bb38f93a48c8
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-14 08:29:35 -0300

    Update default code format style

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
   env: {
     node: true
   },
+  'plugins': [
+    'es-beautifier'
+  ],
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:es-beautifier/standard',
+    'eslint:recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'es-beautifier/multiline-import-specifiers': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
