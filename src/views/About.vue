d709f26418900f8bd3464f5029efe8c2d9c75332
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-14 08:27:16 -0300

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,26 +2,20 @@
   <div class="container">
     <div class="row">
       <div class="col text-center">
-        <h1 class="my-4">My Products</h1>
-        <p>This app was developed using Vue JS.<p>
+        <h1 class="my-4">My Food</h1>
+        <p>This app was developed using Vue JS.</p>
         <p>It comunicates with a back end RESTful API developed in PHP using Laravel/Lumen.</p>
         <p>
           <strong>Version:</strong> 0.1.0
         </p>
         <p>
-          <a
-            href="https://github.com/pedro1191/my-products-vue.git"
-            target="_blank"
-            rel="noopener">
-            Vue JS App source code
+          <a class="text-danger" href="https://github.com/pedro1191/my-products-vue.git" target="_blank" rel="noreferrer noopener">
+            See source code of this Vue JS App
           </a>
         </p>
         <p>
-          <a
-            href="https://github.com/pedro1191/my-products-rest.git"
-            target="_blank"
-            rel="noopener">
-            Laravel/Lumen RESTful API source code
+          <a class="text-danger" href="https://github.com/pedro1191/my-products-rest.git" target="_blank" rel="noreferrer noopener">
+            See source code of the Laravel/Lumen RESTful API
           </a>
         </p>
       </div>
