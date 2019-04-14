657dc01987a3dcfefe87e4d2d00261cef758b4e9
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-14 12:25:15 -0300

    Update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-lg-3">
+      <div class="col-lg-3" v-if="showCategoriesMenu">
         <gws-categories :categories="categories"></gws-categories>
       </div>
       <div class="col-lg-9">
@@ -9,96 +9,107 @@
         <gws-products-cards :products="products"></gws-products-cards>
       </div>
     </div>
+
     <gws-modal v-if="modal.error">
-      <div slot="header"> My Products</div>
-      <div slot="body">{{ modal.message }}</div>
-      <button class="btn btn-primary" @click="onModalClose" slot="footer">OK</button>
-    </gws-modal>
-    <gws-modal v-if="modal.loading">
-      <gws-spinner slot="body"></gws-spinner>
+      <div class="local-modal-header" slot="header"> My Food</div>
+      <div class="local-modal-body" slot="body">{{ modal.message }}</div>
+      <div class="local-modal-footer" slot="footer">
+        <button class="btn btn-secondary" @click="onModalClose">OK</button>
+      </div>
     </gws-modal>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import axios from '@/axios-default'
-import Categories from '@/components/Categories.vue'
-import Slider from '@/components/Carousel/Carousel.vue'
-import ProductsCards from '@/components/Product/Cards.vue'
-import Modal from '@/components/Modal.vue'
-import Spinner from '@/components/Spinner.vue'
+import axios from '@/axios-default';
+import Categories from '../components/Categories.vue';
+import Slider from '../components/Carousel/Carousel.vue';
+import ProductsCards from '../components/Product/Cards.vue';
+import Modal from '../components/Modal.vue';
 
 export default {
-  name: 'home',
-  created () {
-    this.getCategories()
-    this.getProducts()
+  created() {
+    this.getProducts();
   },
-  data () {
+  data() {
     return {
-      categories: [],
       products: [],
       modal: {
-        loading: false,
         error: false,
         message: null
       }
+    };
+  },
+  computed: {
+    showCategoriesMenu: function() {
+      return !this.$store.getters.isASmallDevice;
+    },
+    categories: function() {
+      return this.$store.getters.categories;
     }
   },
   methods: {
-    getCategories () {
-      this.modal.loading = true
-
-      axios.get('/categories')
-        .then(response => {
-          this.modal.loading = false
-          this.categories = response.data.data
-        })
-        .catch(error => {
-          this.onHttpRequestError(error)
-        })
-    },
-    getProducts () {
-      this.modal.loading = true
+    getProducts() {
+      this.$store.dispatch('setLoading', true);
 
       const params = {
         params: {
           include: 'category'
         }
-      }
+      };
 
-      axios.get('/products', params)
+      axios
+        .get('/products', params)
         .then(response => {
-          this.modal.loading = false
-          this.products = response.data.data
+          this.onStopLoading();
+          this.products = response.data.data;
         })
         .catch(error => {
-          this.onHttpRequestError(error)
-        })
+          this.onHttpRequestError(error);
+        });
     },
-    onHttpRequestError (error) {
-      this.modal.loading = false
-      this.modal.error = true
-      console.log(error.response)
+    onHttpRequestError(error) {
+      this.onStopLoading();
+      this.modal.error = true;
 
       switch (error.response.status) {
         default:
-          this.modal.message = 'Oops! Something went wrong.'
+          this.modal.message = 'Oops! Something went wrong.';
       }
     },
-    onModalClose () {
-      this.modal.loading = false
-      this.modal.message = null
-      this.modal.error = false
+    onStopLoading() {
+      this.$store.dispatch('setLoading', false);
+    },
+    onModalClose() {
+      this.modal.message = null;
+      this.modal.error = false;
     }
   },
   components: {
     gwsCategories: Categories,
     gwsSlider: Slider,
     gwsProductsCards: ProductsCards,
-    gwsModal: Modal,
-    gwsSpinner: Spinner
+    gwsModal: Modal
   }
-}
+};
 </script>
+
+<style scoped>
+.local-modal-header,
+.local-modal-body,
+.local-modal-footer {
+  padding: 0.5rem;
+  background-color: #fff;
+  width: 100%;
+}
+
+.local-modal-header {
+  text-align: left;
+  color: #dc3545;
+  font-weight: bold;
+}
+
+.local-modal-footer {
+  text-align: right;
+}
+</style>
