8ba7ac4879a469732c2aa75b64cd7662498612a0
Pedro Almeida <pedro_almeida1191@hotmail.com>
2021-04-03 07:25:34 -0300

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import Vuelidate from 'vuelidate'
+import VueGtag from 'vue-gtag'
 
 Vue.config.productionTip = false
 Vue.use(Vuelidate)
+Vue.use(VueGtag, {
+  config: { id: 'G-63LY7WVZ1P' }
+}, router)
 
 new Vue({
   router,
