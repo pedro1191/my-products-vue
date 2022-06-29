cd8af4e8748eb17fd9a21d2f30f830b910e2e42d
Pedro Almeida <pedro_almeida1191@hotmail.com>
2022-06-29 12:04:44 -0300

    ci: add test workflow

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -16,6 +16,11 @@ module.exports = (on, config) => {
   // }))
 
   return Object.assign({}, config, {
+    env: {
+      VUE_APP_DASHBOARD_APP_URL: process.env.VUE_APP_DASHBOARD_APP_URL,
+      VUE_APP_DEFAULT_BACKEND_API_URL:
+        process.env.VUE_APP_DEFAULT_BACKEND_API_URL,
+    },
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
     screenshotsFolder: 'tests/e2e/screenshots',
